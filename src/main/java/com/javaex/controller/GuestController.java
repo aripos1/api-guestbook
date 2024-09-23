6172287ee150387c19e285cc3c0f29eb954661c4
aripos1 <ihsjjang@gmail.com>
2024-09-23 22:49:42 +0900

    20240923

--- src/main/java/com/javaex/controller/GuestController.java
+++ src/main/java/com/javaex/controller/GuestController.java
@@ -3,42 +3,54 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.GuestbookService;
 import com.javaex.vo.GuestVo;
-
 
 @RestController
 public class GuestController {
 
 	@Autowired
 	private GuestbookService guestbookService;
-	
+
 	@GetMapping("/api/guestbooks")
 	public List<GuestVo> getList() {
-		
+
 		List<GuestVo> guestList = guestbookService.exeGuestList();
-		
+
 		System.out.println(guestList);
 		return guestList;
 	}
-	
+
 	@PostMapping("/api/guestbooks")
 	public String write(@RequestBody GuestVo geustVo) {
 
-//		int count = phonebookDao.insertPerson(personVo);
 		guestbookService.exeWrite(geustVo);
-//		System.out.println(count);
 
 		return "redirect:/";
 	}
-	
-	
-	
-	
-	
+
+	@GetMapping("/api/guestbooks/{no}")
+	public GuestVo getGuestOne(@PathVariable(value = "no") int no) {
+
+		GuestVo guestVo = guestbookService.exeDeleteForm(no);
+
+		System.out.println(guestVo);
+		return guestVo;
+	}
+
+	@DeleteMapping("/api/guestbooks/{no}")
+	public boolean delete(@PathVariable(value = "no") int no, @RequestBody GuestVo guestVo) {
+		String password = guestVo.getPassword(); // 요청에서 비밀번호 가져오기
+		boolean deleted = guestbookService.exeDelete(no, password);
+		return deleted;
+	}
+
 }
